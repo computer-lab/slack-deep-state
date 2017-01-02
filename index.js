db532922123423f19d29e298636ca2b4fa398b64
Robert C Jensen <rob@computerlab.io>
2017-01-02 14:39:00 -0500

    chore: restructure entry point
    * Un-nests RTM event handlers
    * Uses exit code of `1` for missing slack bot token

--- index.js
+++ index.js
@@ -6,16 +6,18 @@ const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 const censor = require('./lib/censor');
 
 const botToken = process.env.SLACK_BOT_TOKEN || '';
-if (!botToken) { console.error('error: Please set SLACK_BOT_TOKEN'); process.exit(-1); }
+if (!botToken) { console.error('error: Please set SLACK_BOT_TOKEN'); process.exit(1); }
 
 const rtm = new RtmClient(botToken, {autoReconnect: false});
 rtm.start();
 
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
   console.error(`info: Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}`);
-  rtm.on(RTM_EVENTS.MESSAGE, function (messageEvent) {
-    if (messageEvent.subtype && messageEvent.subtype === 'message_deleted') {
-      rtm.sendMessage(censor(messageEvent.previous_message.text), messageEvent.channel);
-    }
-  });
+
+});
+
+rtm.on(RTM_EVENTS.MESSAGE, function (messageEvent) {
+  if (messageEvent.subtype && messageEvent.subtype === 'message_deleted') {
+    rtm.sendMessage(censor(messageEvent.previous_message.text), messageEvent.channel);
+  }
 });
