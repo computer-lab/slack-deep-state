361198d649ff54cc4bc4fd336f4db39a1a7fdcd5
Robert C Jensen <rob@computerlab.io>
2017-01-31 01:27:23 -0500

    feature: minimum viable multitenancy

--- bin/slack-deep-state.js
+++ bin/slack-deep-state.js
@@ -1,2 +1,28 @@
 #!/usr/bin/env node
-require('..');
+
+if (process.argv.length < 4) {
+  console.log('usage: slack-deep-state <host> <port>');
+  process.exit(1);
+}
+
+const http = require('http');
+const deepState = require('..');
+
+const server = http.createServer((req, res) => {
+  const token = req.headers['x-slack-token'];
+  console.log(token);
+  deepState(token);
+  res.end();
+});
+
+const host = process.argv[2];
+const port = process.argv[3];
+
+server.listen(port, host, function () {
+  console.log(`${host} ${port} starting`)
+});
+
+process.on('SIGINT', function () {
+  console.log(`${host} ${port} stopping`)
+  process.exit(0);
+});
--- index.js
+++ index.js
@@ -5,19 +5,17 @@ const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 const censor = require('./lib/censor');
 
-const botToken = process.env.SLACK_BOT_TOKEN || '';
-if (!botToken) { console.error('error: Please set SLACK_BOT_TOKEN'); process.exit(1); }
+module.exports = function (token) {
+  const rtm = new RtmClient(token, {autoReconnect: false});
+  rtm.start();
 
-const rtm = new RtmClient(botToken, {autoReconnect: false});
-rtm.start();
+  rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
+    console.error(`info: Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}`);
+  });
 
-rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
-  console.error(`info: Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}`);
-
-});
-
-rtm.on(RTM_EVENTS.MESSAGE, function (messageEvent) {
-  if (messageEvent.subtype && messageEvent.subtype === 'message_deleted') {
-    rtm.sendMessage(censor(messageEvent.previous_message.text), messageEvent.channel);
-  }
-});
+  rtm.on(RTM_EVENTS.MESSAGE, function (messageEvent) {
+    if (messageEvent.subtype && messageEvent.subtype === 'message_deleted') {
+      rtm.sendMessage(censor(messageEvent.previous_message.text), messageEvent.channel);
+    }
+  });
+};
